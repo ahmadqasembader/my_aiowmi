f9dcf507bc1f3d8553abf17c52acf8d98a8b5616
ahmad bader <ahmadbader245@gmail.com>
2023-12-04 12:49:08 +0300

    pack data in little endian format

--- ndr/com_version.go
+++ ndr/com_version.go
@@ -2,12 +2,26 @@ package ndr
 
 // MS-DCOM 2.2.11 [COMVERSION]
 
-var COM_VESION_MAJOR uint = 7
-var COM_VESION_MINOR uint = 5
+/*
+	The COMVERSION structure is used to specify the major and minor version of either the client or the
+	server DCOM Remote Protocol implementation.
+*/
 
+import (
+	"encoding/binary"
+)
+
+var COM_VERSION_MAJOR uint = 7
+var COM_VERSION_MINOR uint = 5
+
+// returns major and midor version of COM in little endian byte format. 
 func GetCOMVersion()[]byte {
 	var buffer []byte
-	buffer = append(buffer, byte(COM_VESION_MAJOR), byte(COM_VESION_MAJOR>>8))
-	buffer = append(buffer, byte(COM_VESION_MINOR), byte(COM_VESION_MINOR>>8))
+
+	value := binary.LittleEndian.Uint16([]byte{byte(COM_VERSION_MAJOR), byte(COM_VERSION_MAJOR>>8)})	
+	buffer = append(buffer, byte(value), byte(value>>8))
+
+	value = binary.LittleEndian.Uint16([]byte{byte(COM_VERSION_MINOR), byte(COM_VERSION_MINOR>>8)})
+	buffer = append(buffer, byte(value), byte(value>>8))
 	return buffer
 }
