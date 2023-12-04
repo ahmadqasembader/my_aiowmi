8bf0b1df140924006daa7d12f272c1ee9c6a1fc0
ahmad bader <ahmadbader245@gmail.com>
2023-12-04 14:42:39 +0300

    make struct literal exportable

--- ndr/orpcthat.go
+++ ndr/orpcthat.go
@@ -5,19 +5,19 @@ import (
 	"fmt"
 )
 type ORPCTHAT struct {
-	flags      uint
-	extensions uint
+	Flags      uint
+	Extensions uint
 }
 
-func (orpcthat *ORPCTHAT) from_data(data []byte, offset int) (int, error){
+func (orpcthat *ORPCTHAT) From_data(data []byte, offset int) (int, error){
 	offset = 0
 
 	if len(data)-offset < 8 {
 		return offset, fmt.Errorf("insufficient data for unpacking")
 	}
 
-	orpcthat.flags = uint(binary.LittleEndian.Uint32(data[ offset : offset+4 ]))
-	orpcthat.extensions = uint(binary.LittleEndian.Uint32(data[ offset+4 : offset+8 ]))
+	orpcthat.Flags = uint(binary.LittleEndian.Uint32(data[ offset : offset+4 ]))
+	orpcthat.Extensions = uint(binary.LittleEndian.Uint32(data[ offset+4 : offset+8 ]))
 
 	// TODO: support to the exenstions
 	return offset + 8, nil
