d5282ca92cc42520d2a12c0b5af45ef03ec0d72b
ahmad bader <ahmadbader245@gmail.com>
2023-12-05 11:59:04 +0300

    Test case for orpcthis class

--- tests/orpc_test.go
+++ tests/orpc_test.go
@@ -2,6 +2,9 @@ package test
 
 import (
 	"aiowmi/ndr"
+	"aiowmi/tools"
+	"bytes"
+	"fmt"
 	"testing"
 )
 
@@ -26,4 +29,40 @@ func TestOrpcthat(t *testing.T){
 		t.Errorf("Offset is incorrect: %d -- %v", offset, err)
 		panic(err)
 	}
+}
+
+// The FixedCID function is used instead of GenCID for testing purposes
+// The test will fail if you don't fix the CID in the orpcthis.go as well
+func TestOrpcthis(t *testing.T) {
+	// Test case 1: Flags = 42
+	flags1 := 43
+
+	orpcthis1 := ndr.ORPCTHIS{}
+	result1 := orpcthis1.From_data(flags1)
+
+	expectedData1 := append([]byte{0, 0, 0, 0, 0, 0, 0, 0}, ndr.GetCOMVersion()...)
+	expectedData1 = append(expectedData1, tools.FixedCID()...)
+	
+	if !bytes.Equal(result1, expectedData1) {
+		t.Errorf("Unexpected result for test case 1. Got %+v, expected %+v", result1, expectedData1)
+	}else{
+		fmt.Printf("Expected Output: %d - Acutal Output: %d \n", expectedData1, result1)
+	}
+
+	// Test case 2: Flags = 0
+	flags2 := 0
+
+	orpcthis2 := ndr.ORPCTHIS{}
+	result2 := orpcthis2.From_data(flags2)
+
+	
+	expectedData2 := append([]byte{0, 0, 0, 0, 0, 0, 0, 0}, ndr.GetCOMVersion()...)
+	expectedData2 = append(expectedData2, tools.FixedCID()...)
+
+	if !bytes.Equal(result2, expectedData2) {
+		t.Errorf("Unexpected result for test case 2. Got %+v, expected %+v", result2, expectedData2)
+	}else{
+		fmt.Printf("Expected Output: %d - Acutal Output: %d \n", expectedData2, result2)
+	}
+
 }
